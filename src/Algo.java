3116c0c85ec6e3d5c818d7ddc9d4d58006bd75b0
Sandrine <sandrine.yven@gmail.com>
2018-11-19 19:40:15 -0500

    declaration variables

--- src/Algo.java
+++ src/Algo.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Algo {
 	//Paramètres
-	private static final double qtot = 15;
-	private static final double elam = 172;
+	private static final int qtot = 15;
+	private static final double elam = 175;
 	
 	//Coefficients de l'équation de l'élévation aval
 	private static double[] coeffElav = {-7.071e-07, 0.004118, 137.2};
@@ -11,31 +11,56 @@ public class Algo {
 	//Coefficients de l'équation des puissance des 5 turbines
 	private static double[] coeffP1 = {31.57, 1.796, 17.28, - 0.0078120, 0.8945, -2.914, -0.014010, 0.12623};
 	private static double[] coeffP2 = {30.33, 1.715, 20.53, - 0.0064880, 1.0850, -3.356, 0.020120,  0.11620};
-	private static double[] coeffP3 = {33.58, 1.891, 16.88, - 0.0007952, 1.0010, -3.611, -0.007118, 0.1408};
-	private static double[] coeffP4 = {34.50, 1.893, 19.79, - 0.0061180, 1.2370, -4.030, 0.007646,  0.17570};
+	private static double[] coeffP3 = {33.58, 1.891, 16.88, - 0.0007952, 1.0010, -3.611, -0.007118, 0.14080};
+	private static double[] coeffP4 = {34.50, 1.893, 19.79, - 0.0061180, 1.2370, -4.030, 0.007646,  0.15470};
 	private static double[] coeffP5 = {32.00, 2.030, 17.86, - 0.0282700, 1.2390, -2.710, 0.034180,  0.11840};
 	
 
 	public static void main(String[] args) {
-		double qn = 155;
-		double hn =  calculHauteurChuteNette(qn);
-		for(int n = 1; n<6; n++) {
-			double p = calculPuissance(qn, hn, n);
-			System.out.println("Puissance turbine " + n + " : " + p);
+		
+		//Discrétisation de Sn
+		int sizeSn = qtot/5 +1;
+		int[] Sn = new int[sizeSn];
+		int[] Qn = new int[sizeSn];
+		int indexSn = 0;
+		for(int i= 0; i<=qtot; i +=5) {
+			//Débit restant
+			Sn[indexSn] = i;
+			//Débit attribué
+			Qn[indexSn] = i;
+			indexSn++;
 		}
+		
+		double[][] Gn = new double[6][sizeSn];
+		
+		//Backward pass
+
+		//Turbine 5:
+			for(int sn = 0; sn<sizeSn; sn++) {
+				double hn =  calculHauteurChuteNette(Sn[sn]);
+				double p = calculPuissance(Sn[sn], hn, 5);
+				Gn[5][sn] = p;
+				System.out.println("Puissance turbine  : " + Gn[5][sn]);
+				System.out.println("Sn: " + Sn[sn]);
+			}
+
+	
 	}
 
 	private static double calculHauteurChuteNette(double qn) {
 		double perte = 5*0.00001*qn*qn;
 		double elav = coeffElav[0] * qn * qn + coeffElav[1] * qn + coeffElav[2];
 		double hn = elam - elav - perte;
-		System.out.println("Hauteur de chutte nette: "+hn);
+		//System.out.println("Hauteur de chutte nette: "+hn);
 		return hn;
 	}
 	
 	private static double calculPuissance(double qn, double hn, int n) {
 		
 		double puissance = 0;
+		if(qn == 0) {
+			return puissance;
+		}
 		switch(n) {
 			case 1: puissance = coeffP1[0] + (coeffP1[1] * hn) + (coeffP1[2] * qn) 
 					+ (coeffP1[3] * hn * hn) + (coeffP1[4] * hn * qn) + (coeffP1[5] * qn * qn) 
