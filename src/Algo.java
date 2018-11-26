7b4cfdb4e28ef0df2bea96a3834bd3779a968e7e
Sandrine <sandrine.yven@gmail.com>
2018-11-26 12:51:55 -0500

    Code fonctionel final

--- src/Algo.java
+++ src/Algo.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 
 public class Algo {
-	
+
 	public static ArrayList<Turbine> listeTurbines = new ArrayList<Turbine>(6);
-	
-	public static void main(String[] args) {		
-		
+
+	public static void main(String[] args) {
+
 		// Discrétisation de Sn
 		int sizeSn = Constante.qtot / 5 + 1;
+		int sizeXn = Constante.debitMaxTurbine / 5 + 1;
 		int[] Sn = new int[sizeSn];
 		int indexSn = 0;
 		for (int i = 0; i <= Constante.qtot; i += 5) {
@@ -16,146 +17,166 @@ public class Algo {
 			indexSn++;
 		}
 
-		// Initialisation des turbines - TRUE: ACTIVATION - FALSE: désactivation
+		// Initialisation des turbines - TRUE: ACTIVATION - FALSE: DESACTIVATION
 		listeTurbines.add(null);
 		Turbine turbine1 = new Turbine(sizeSn, Sn, 1, true);
-		if(turbine1.isActive()) listeTurbines.add(turbine1);
+		if (turbine1.isActive())
+			listeTurbines.add(turbine1);
 		Turbine turbine2 = new Turbine(sizeSn, Sn, 2, true);
-		if(turbine2.isActive()) listeTurbines.add(turbine2);
+		if (turbine2.isActive())
+			listeTurbines.add(turbine2);
 		Turbine turbine3 = new Turbine(sizeSn, Sn, 3, true);
-		if(turbine3.isActive())listeTurbines.add(turbine3);
+		if (turbine3.isActive())
+			listeTurbines.add(turbine3);
 		Turbine turbine4 = new Turbine(sizeSn, Sn, 4, false);
-		if(turbine4.isActive())listeTurbines.add(turbine4);
+		if (turbine4.isActive())
+			listeTurbines.add(turbine4);
 		Turbine turbine5 = new Turbine(sizeSn, Sn, 5, true);
-		if(turbine5.isActive())listeTurbines.add(turbine5);
+		if (turbine5.isActive())
+			listeTurbines.add(turbine5);
 
-		// Backward pass
-		// Turbine 5 (premiere passe):
+		// Backward pass------------------------------------------------------
+
+		// Première passe:
 		for (int sn = 1; sn < sizeSn; sn++) {
-			
-			if(Sn[sn] > Constante.debitMaxTurbine) {
-				listeTurbines.get(listeTurbines.size() - 1).setFnmax(0,sn);
-			}else {
-				listeTurbines.get(listeTurbines.size() - 1).setFnmax(turbine5.getGn(sn),sn);
+			if (Sn[sn] > Constante.debitMaxTurbine) {
+				listeTurbines.get(listeTurbines.size() - 1)
+						.setFnmax(listeTurbines.get(listeTurbines.size() - 1).getGn(Constante.debitMaxTurbine / 5), sn);
+				listeTurbines.get(listeTurbines.size() - 1).setXn(Constante.debitMaxTurbine, sn);
+			} else {
+				listeTurbines.get(listeTurbines.size() - 1)
+						.setFnmax(listeTurbines.get(listeTurbines.size() - 1).getGn(sn), sn);
 				listeTurbines.get(listeTurbines.size() - 1).setXn(Sn[sn], sn);
 			}
-			//System.out.println("turbine: " +listeTurbines.get(listeTurbines.size() - 1).getId() + " Fninter: " + listeTurbines.get(listeTurbines.size() - 1).getFnmax(sn) + " sn :" + sn);
 		}
-		
+
 		// Turbines intermédiaires:
 		// FnInter: Tableau contenant tous les cas possibles
 		double[][] FnInter = new double[sizeSn][sizeSn];
 		FnInter[0][0] = 0;
-		
-		// Pour toutes les turbines intermediaires: (ici de 4 à 2)
-		for (int turbine = listeTurbines.size()-2; turbine > 1; turbine--) {
+
+		// Pour toutes les turbines intermediaires: (ici de 4 à 2 si toutes actives)
+		for (int turbine = listeTurbines.size() - 2; turbine > 1; turbine--) {
+			// Case sn =0, xn =0 initialisée à 0:
 			listeTurbines.get(turbine).setFnmax(0, 0);
 			listeTurbines.get(turbine).setXn(0, 0);
 			for (int sn = 1; sn < sizeSn; sn++) {
-				for (int xn = 0; xn < sizeSn; xn++) {
-					if(Sn[sn] > Constante.debitMaxTurbine || Sn[xn] > Constante.debitMaxTurbine ) {
-						FnInter[sn][xn] = listeTurbines.get(turbine).getGn(Constante.debitMaxTurbine/5) + listeTurbines.get(turbine+1).getFnmax(Constante.debitMaxTurbine/5);
-					}else if (sn - xn < 0) {
+				for (int xn = 0; xn < sizeXn; xn++) {
+					if (sn - xn < 0) {
 						// zone non remplissable du tableau
 						FnInter[sn][xn] = -1;
 					} else {
-						FnInter[sn][xn] = listeTurbines.get(turbine).getGn(xn) + listeTurbines.get(turbine+1).getFnmax(sn-xn);
+						FnInter[sn][xn] = listeTurbines.get(turbine).getGn(xn)
+								+ listeTurbines.get(turbine + 1).getFnmax(sn - xn);
 					}
-				//	System.out.println("turbine: "+ turbine + " Fninter: " + FnInter[sn][xn] + " sn :" + sn + " xn: " + xn +" Fnmax: " + listeTurbines.get(turbine).getFnmax(sn));
-					// Trouver les maxi: reconstruction de Fnmax pour le calcul de la turbine
-					// précédente et Recupération du débit pour le quel Fnmax est maximum (pour l'utiliser dans le
-					// forward pass)
-					if (listeTurbines.get(turbine).getFnmax(sn) < FnInter[sn][xn]) {
+				}
+			}
+
+			// Trouver les maxi + Recupération du débit pour le quel Fnmax est maximum (pour
+			// l'utiliser dans le forward pass)
+			for (int sn = 1; sn < sizeSn; sn++) {
+				for (int xn = 0; xn < sizeXn; xn++) {
+					if (listeTurbines.get(turbine).getFnmax(sn) <= FnInter[sn][xn]) {
 						// Récupération du maximum
 						listeTurbines.get(turbine).setFnmax(FnInter[sn][xn], sn);
 						// Récupération du débit correspondant au maximum
-						listeTurbines.get(turbine).setXn(Sn[xn],sn);
+						listeTurbines.get(turbine).setXn(Sn[xn], sn);
 					}
-
-					
-					//System.out.println("turbine: " + turbine +" Fnmax: " + turbineCourante.getFnmax(sn) + " qn: " + turbineCourante.getXn(sn));
 				}
 			}
 		}
 
-		// Turbine 1:
-
-		//System.out.println("turbine: " + 1 +" Fnmax: " + listeTurbines.get(1).getFnmax(0) + " qn: " + listeTurbines.get(1).getXn(0));
-		double temp= 0;
-		for (int xn = 1; xn < sizeSn; xn++) {
-			if(Sn[xn] > Constante.debitMaxTurbine) {
-				listeTurbines.get(1).setFnmax(0,xn);
-			}else {
-				temp = listeTurbines.get(1).getGn(xn) + listeTurbines.get(2).getFnmax(sizeSn - 1 - xn);
-				if (temp > listeTurbines.get(1).getFnmax(xn)) {
+		// Dernière passe:
+		double temp = 0;
+		listeTurbines.get(1).setFnmax(0, 0);
+		for (int xn = 1; xn < sizeXn; xn++) {
+			temp = listeTurbines.get(1).getGn(xn) + listeTurbines.get(2).getFnmax(sizeSn - 1 - xn);
+			if (temp >= listeTurbines.get(1).getFnmax(sizeSn - 1)) {
 				// Récupération du maximum
-				listeTurbines.get(1).setFnmax(temp,  sizeSn - 1);
+				listeTurbines.get(1).setFnmax(temp, sizeSn - 1);
 				// Récupération du débit correspondant au maximum
 				listeTurbines.get(1).setXn(Sn[xn], sizeSn - 1);
-				}
 			}
-			System.out.println("turbine: " + 1 +" Fnmax: " + listeTurbines.get(1).getFnmax(xn) + " qn: " + listeTurbines.get(1).getXn(xn));
 		}
-		
-		// Forward pass
 
-		int Q1 = turbine1.getXn(sizeSn-1);
-		int index = Q1/5 > 0 ? Q1/5 : 0;
-		double P1 = turbine1.getFnmax(index);
+		// Forward pass-----------------------------------------------
+
+		
+		//Turbine 1:
+		int Q1 = turbine1.getXn(sizeSn - 1);
+		int index = Q1 / 5 > 0 ? Q1 / 5 : 0;
+		double P1 = turbine1.getGn(index);
 		int debitRestant = Constante.qtot - Q1;
-		System.out.println("q1 = "  + Q1);
-		System.out.println(" debit restant : " + debitRestant );
-		System.out.println("P1 : " + P1);
-
-		int debitInter = debitRestant>Constante.debitMaxTurbine ? Constante.debitMaxTurbine:debitRestant;
-		index = debitInter/5 > 0 ? debitInter/5 : 0;
+		turbine1.setDebitFinal(Q1);
+		turbine1.setPuissanceFinale(P1);
 		
+		index = debitRestant / 5 > 0 ? debitRestant / 5 : 0;
+
+		//Turbine 2:
 		int Q2 = turbine2.getXn(index);
-		index =   Q2/5 > 0 ? Q2/5 : 0;
-		double P2 = turbine2.getFnmax(index);
+		index = Q2 / 5 > 0 ? Q2 / 5 : 0;
+		double P2 = turbine2.getGn(index);
 		debitRestant -= Q2;
-		System.out.println("q2 = "  + Q2);
-		System.out.println(" debit restant : " + debitRestant );
-		System.out.println("P2 : " + P2);
+		turbine2.setDebitFinal(Q2);
+		turbine2.setPuissanceFinale(P2);
 
-		debitInter = debitRestant>Constante.debitMaxTurbine ? Constante.debitMaxTurbine:debitRestant;
-		index = debitInter/5 > 0 ? debitInter/5 : 0;
-		
+		index = debitRestant / 5 > 0 ? debitRestant / 5 : 0;
+
+		//Turbine 3:
 		int Q3 = turbine3.getXn(index);
-		index =   Q3/5 > 0 ? Q3/5 : 0;
-		double P3 = turbine3.getFnmax(index);
+		index = Q3 / 5 > 0 ? Q3 / 5 : 0;
+		double P3 = turbine3.getGn(index);
 		debitRestant -= Q3;
-		System.out.println("q3 = "  + Q3);
-		System.out.println(" debit restant : " + debitRestant );
-		System.out.println("P3 : " + P3);
-		
-		debitInter = debitRestant>Constante.debitMaxTurbine ? Constante.debitMaxTurbine:debitRestant;
-		index = debitInter/5 > 0 ? debitInter/5 : 0;
-		
+		turbine3.setDebitFinal(Q3);
+		turbine3.setPuissanceFinale(P3);
+
+		index = debitRestant / 5 > 0 ? debitRestant / 5 : 0;
+
+		//Turbine 4:
 		int Q4 = turbine4.getXn(index);
-		index =   Q4/5 > 0 ? Q4/5 : 0;
-		double P4 = turbine4.getFnmax(index);
+		index = Q4 / 5 > 0 ? Q4 / 5 : 0;
+		double P4 = turbine4.getGn(index);
 		debitRestant -= Q4;
-		System.out.println("q4 = "  + Q4);
-		System.out.println(" debit restant : " + debitRestant );
-		System.out.println("P4 : " + P4);
-		
-		debitInter = debitRestant>Constante.debitMaxTurbine ? Constante.debitMaxTurbine:debitRestant;
-		index = debitInter/5 > 0 ? debitInter/5 : 0;
-		
+		turbine4.setDebitFinal(Q4);
+		turbine4.setPuissanceFinale(P4);
+
+		index = debitRestant / 5 > 0 ? debitRestant / 5 : 0;
+
+		//Turbine 5:
 		int Q5 = turbine5.getXn(index);
-		index =   Q5/5 > 0 ? Q5/5 : 0;
-		double P5 = turbine5.getFnmax(index);
+		index = Q5 / 5 > 0 ? Q5 / 5 : 0;
+		double P5 = turbine5.getGn(index);
 		debitRestant -= Q5;
-		System.out.println("q5 = "  + Q5);
-		System.out.println(" debit restant : " + debitRestant );
-		System.out.println("P5 : " + P5);
-		
+		turbine5.setDebitFinal(Q5);
+		turbine5.setPuissanceFinale(P5);
+
 		double Ptot = P1 + P2 + P3 + P4 + P5;
+		
+		//Affichage des résultats:
+		
+		System.out.println("Turbine n°1:");
+		System.out.println("Q1:  " + turbine1.getDebitFinal());
+		System.out.println("P1:  " + turbine1.getPuissanceFinale());
+		
+		System.out.println("Turbine n°2:");
+		System.out.println("Q2:  " + turbine2.getDebitFinal());
+		System.out.println("P2:  " + turbine2.getPuissanceFinale());
+		
+		System.out.println("Turbine n°3:");
+		System.out.println("Q3:  " + turbine3.getDebitFinal());
+		System.out.println("P3:  " + turbine3.getPuissanceFinale());
+		
+		System.out.println("Turbine n°4:");
+		System.out.println("Q4:  " + turbine4.getDebitFinal());
+		System.out.println("P4:  " + turbine4.getPuissanceFinale());
+		
+		System.out.println("Turbine n°5:");
+		System.out.println("Q5:  " + turbine5.getDebitFinal());
+		System.out.println("P5:  " + turbine5.getPuissanceFinale());
+		
+		
 		System.out.println("Puissance max opti:  " + Ptot);
+		System.out.println("Débit restant:  " + debitRestant);
 	}
 
-
-
-	
 }
--- src/Turbine.java
+++ src/Turbine.java
@@ -2,13 +2,12 @@
 public class Turbine {
 
 	// Coefficients de l'équation des puissance des 5 turbines
-	private static double[] coeffP1 = { 31.57, 1.796, 17.28, -0.0078120, 0.8945, -2.914, -0.014010, 0.12623,-0.1* 1.918 };
-	private static double[] coeffP2 = { 30.33, 1.715, 20.53, -0.0006488, 1.0850, -3.356, -0.020120, 0.10620,-0.1* 3.021 };
-	private static double[] coeffP3 = { 33.58, 1.891, 16.88, -0.0007952, 1.0010, -3.611, -0.007118, 0.09408, -0.1* 2.002 };
-	private static double[] coeffP4 = { 34.50, 1.893, 19.79, -0.0061180, 1.2370, -4.030, -0.007646, 0.15470, -0.1* 2.676 };
-	private static double[] coeffP5 = { 32.00, 2.030, 17.86, -0.0028270, 1.2390, -2.710,  0.034180, 0.11840, -0.1* 1.904 };
-	
-	
+	private static double[] coeffP1 = {  0.08651, -0.0025430, -0.1976, 0.008183, 0.002892, 7.371 * 0.000001, -1.191 * 0.00001 };
+	private static double[] coeffP2 = {  0.81220, -0.0237400, -0.2442, 0.006492, 0.003838, 2.207 * 0.000010, -1.665 * 0.00001 };
+	private static double[] coeffP3 = { -0.02446,  0.0009464, -0.2157, 0.006353, 0.003541, 2.214 * 0.000010, -1.569 * 0.00001 };
+	private static double[] coeffP4 = { -0.04632,  0.0017690, -0.1905, 0.004951, 0.003537, 3.487 * 0.000010, -1.689 * 0.00001 };
+	private static double[] coeffP5 = {  0.29460, -0.0080740, -0.1834, 0.008090, 0.002706, 1.949 * 0.000010, -1.318 * 0.00001 };
+
 	// Puissances de base
 	private double[] Gn;
 
@@ -17,7 +16,7 @@ public class Turbine {
 	// Débit pour le quel la solution est optimale
 	private int[] Xn;
 
-	// Id de la turbine
+	// propriétés de la turbine
 	private int id;
 	private boolean isActive = false;
 	private int debitFinal = 0;
@@ -28,29 +27,26 @@ public class Turbine {
 		this.Gn = initGn(sizeSn, Sn);
 		this.Fnmax = new double[sizeSn];
 		this.Xn = new int[sizeSn];
-		if(active) {
+		if (active) {
 			this.isActive = true;
 		}
 	}
 
+	// Création de la matrice des puissances de base
 	private double[] initGn(int sizeSn, int[] Sn) {
 		double[] Gn = new double[sizeSn];
 		for (int sn = 0; sn < sizeSn; sn++) {
 			if (Sn[sn] > Constante.debitMaxTurbine) {
 				Gn[sn] = 0;
 			} else {
-				double hn = calculHauteurChuteNette(Sn[sn]);
-				double p = calculPuissance(Sn[sn], hn, this.id);
-				Gn[sn] = p > 0 ? p:0;
-				System.out.println("id : " + this.id + " gn: " + Gn[sn] +" sn : " + sn*5);
+				Gn[sn] = calculPuissance(Sn[sn], calculHauteurChuteNette(Sn[sn]), this.id);
 			}
 		}
-		
 		return Gn;
 	}
 
 	private static double calculHauteurChuteNette(double qn) {
-		double perte = 5 * 0.00001 * qn * qn;
+		double perte = 0.5 * 0.00001 * qn * qn;
 		double elav = Constante.coeffElav[0] * Constante.qtot * Constante.qtot + Constante.coeffElav[1] * Constante.qtot
 				+ Constante.coeffElav[2];
 		double hn = Constante.elam - elav - perte;
@@ -65,42 +61,33 @@ public class Turbine {
 		}
 		switch (n) {
 		case 1:
-			puissance = coeffP1[0] + (coeffP1[1] * hn) + (coeffP1[2] * qn) + (coeffP1[3] * hn * hn)
-					+ (coeffP1[4] * hn * qn) + (coeffP1[5] * qn * qn) + (coeffP1[6] * qn * hn * hn)
-					+ (coeffP1[7] * qn * qn * hn) + (coeffP1[8] * qn * qn * qn);
+			puissance = coeffP1[0] + (coeffP1[1] * hn) + (coeffP1[2] * qn) + (coeffP1[3] * hn * qn)
+					+ (coeffP1[4] * qn * qn) + (coeffP1[5] * qn * qn * hn) + (coeffP1[6] * qn * qn * qn);
 			break;
-
 		case 2:
-			puissance = coeffP2[0] + (coeffP2[1] * hn) + (coeffP2[2] * qn) + (coeffP2[3] * hn * hn)
-					+ (coeffP2[4] * hn * qn) + (coeffP2[5] * qn * qn) + (coeffP2[6] * qn * hn * hn)
-					+ (coeffP2[7] * qn * qn * hn)+ (coeffP2[8] * qn * qn * qn);
+			puissance = coeffP2[0] + (coeffP2[1] * hn) + (coeffP2[2] * qn) + (coeffP2[3] * hn * qn)
+					+ (coeffP2[4] * qn * qn) + (coeffP2[5] * qn * qn * hn) + (coeffP2[6] * qn * qn * qn);
 			break;
-
 		case 3:
-			puissance = coeffP3[0] + (coeffP3[1] * hn) + (coeffP3[2] * qn) + (coeffP3[3] * hn * hn)
-					+ (coeffP3[4] * hn * qn) + (coeffP3[5] * qn * qn) + (coeffP3[6] * qn * hn * hn)
-					+ (coeffP3[7] * qn * qn * hn)+ (coeffP3[8] * qn * qn * qn);
+			puissance = coeffP3[0] + (coeffP3[1] * hn) + (coeffP3[2] * qn) + (coeffP3[3] * hn * qn)
+					+ (coeffP3[4] * qn * qn) + (coeffP3[5] * qn * qn * hn) + (coeffP3[6] * qn * qn * qn);
 			break;
 		case 4:
-			puissance = coeffP4[0] + (coeffP4[1] * hn) + (coeffP4[2] * qn) + (coeffP4[3] * hn * hn)
-					+ (coeffP4[4] * hn * qn) + (coeffP4[5] * qn * qn) + (coeffP4[6] * qn * hn * hn)
-					+ (coeffP4[7] * qn * qn * hn)+ (coeffP4[8] * qn * qn * qn);
+			puissance = coeffP4[0] + (coeffP4[1] * hn) + (coeffP4[2] * qn) + (coeffP4[3] * hn * qn)
+					+ (coeffP4[4] * qn * qn) + (coeffP4[5] * qn * qn * hn) + (coeffP4[6] * qn * qn * qn);
 			break;
-
 		case 5:
-			puissance = coeffP5[0] + (coeffP5[1] * hn) + (coeffP5[2] * qn) + (coeffP5[3] * hn * hn)
-					+ (coeffP5[4] * hn * qn) + (coeffP5[5] * qn * qn) + (coeffP5[6] * qn * hn * hn)
-					+ (coeffP5[7] * qn * qn * hn)+ (coeffP5[8] * qn * qn * qn);
+			puissance = coeffP5[0] + (coeffP5[1] * hn) + (coeffP5[2] * qn) + (coeffP5[3] * hn * qn)
+					+ (coeffP5[4] * qn * qn) + (coeffP5[5] * qn * qn * hn) + (coeffP5[6] * qn * qn * qn);
 			break;
 		default:
 			puissance = 0;
 		}
-
-		// Convertion en MW
-		return puissance *= 0.001;
-
+		return puissance;
 	}
 
+	//Getteurs et setteurs ---------------------------------------------------------------------
+	
 	public double getGn(int i) {
 		return Gn[i];
 	}
@@ -141,5 +128,20 @@ public class Turbine {
 		this.isActive = isActive;
 	}
 
-	
+	public int getDebitFinal() {
+		return debitFinal;
+	}
+
+	public void setDebitFinal(int debitFinal) {
+		this.debitFinal = debitFinal;
+	}
+
+	public double getPuissanceFinale() {
+		return puissanceFinale;
+	}
+
+	public void setPuissanceFinale(double puissanceFinale) {
+		this.puissanceFinale = puissanceFinale;
+	}
+
 }
